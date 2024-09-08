2395ca486f39e83213f77c73098a5a6fb54fbc42
Rajni <balatamna16@gmail.com>
2024-09-08 14:46:38 -0400

    product api updated

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,3 +1,12 @@
 const Product = require("../models/product");
+const mongooseConnectionFile = require("../db/mongoose");
+const jwt = require("jsonwebtoken");
 
+const express = require("express");
+//create a new instance of an Express router
+const router = express.Router();
+
+//to create a unique id
+const { v4: uuidv4 } = require("uuid");
 //Create the product
+
